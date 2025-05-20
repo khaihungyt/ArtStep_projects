0c6942ebde92edf6aa890bede45fd767b4b7f53c
Khải Nguyễn <khaihungyt@gmail.com>
2025-05-20 07:58:28 +0700

    add data

---
+++ ArtStep/Data/Cart.cs
@@ -0,0 +1,11 @@
+﻿namespace ArtStep.Data
+{
+    public class Cart
+    {
+        public string? CartId {  get; set; }
+        
+        public virtual ICollection<User>? Users { get; set; }
+
+        public virtual ICollection<CartDetail>? CartDetails { get; set; }
+    }
+}
--- ArtStep/Program.cs
+++ ArtStep/Program.cs
@@ -1,3 +1,7 @@
+using ArtStep.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +10,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+var connectionString = builder.Configuration.GetConnectionString("MyDatabase");
+builder.Services.AddDbContext<ArtStepDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
